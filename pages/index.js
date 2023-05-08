43d775d0656804dcc6b7f24c065cca8c533c8376
Vaughn <vaughn.dimarco@gmail.com>
2023-05-08 13:02:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,26 @@ export default function Home() {
       <Head>
         <title>Kram Bot!</title>
         <link rel="icon" href="/favicon.ico" />
+     <style>
+      #main {
+        margin-bottom: -250px;
+      }
+      .footer {background: white;
+      }
+      
+    </style>
       </Head>
 
       <main>
         <Header title="Welcome!" />
-        <script>
-  window.chatbaseConfig = {
-    chatbotId: "gMSeV27eEggGqOgifdEwY",
-  }
-</script>
-<script
-  src="https://www.chatbase.co/embed.min.js"
-  id="gMSeV27eEggGqOgifdEwY"
-  defer>
-</script>
-
+         
+        <iframe
+          src="https://www.chatbase.co/chatbot-iframe/gMSeV27eEggGqOgifdEwY"
+          width="100%"
+          height="700"
+          frameborder="0"
+        ></iframe>
+        
 
       </main>
 
